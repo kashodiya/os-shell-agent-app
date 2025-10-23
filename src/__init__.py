45d530754644158310fb6785b629de376c47d9d7
Kaushik Ashodiya <kaushik.ashodiya@sf.frb.org>
2025-10-22 20:40:24 -0700

    reset

--- src/__init__.py
+++
@@ -1,9 +0,0 @@
-
-"""
-OS Shell Agent - AI Agent that can execute tasks using shell commands
-"""
-
-from .shell_agent import ShellAgent
-from .advanced_shell_agent import AdvancedShellAgent
-
-__version__ = "0.1.0"
--- tests/__init__.py
+++
@@ -1,6 +0,0 @@
-
-
-"""
-Tests for the OS Shell Agent
-"""
-
